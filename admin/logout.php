095432b91701408e4ddfc521342d92d3fa2e8b0b
Ashraful Islam Alvee <alvmz32@gmail.com>
2020-02-28 05:59:47 +0600

    admin area customising

--- admin/logout.php
+++ admin/logout.php
@@ -1,2 +1,3 @@
 <center><h1> Please Log in first </h1> </center>
-<button type="submit" formaction="https://www.w3docs.com">Click me</button>
+
+<h1> </h1><button> Click here </button> for login </h1>
